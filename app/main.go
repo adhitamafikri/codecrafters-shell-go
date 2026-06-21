538a3e91316a99021a8779192e90d44dab9fde91
Fikri <adhitamafikri.developer@gmail.com>
2026-06-22 00:33:10 +0700

    feat: minimum implementation, Handle invalid commands

--- app/main.go
+++ app/main.go
@@ -7,7 +7,15 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+func printError(command string) {
+	fmt.Printf("%s: command not found", command)
+}
+
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	var command = ""
+	fmt.Scan(&command)
+	printError(command)
 }
