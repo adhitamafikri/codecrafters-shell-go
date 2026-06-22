ee0925c5672533cb8de6179948d2d21026b7c311
Fikri <adhitamafikri.developer@gmail.com>
2026-06-22 21:03:54 +0700

    feat: implementing exit builtin [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,42 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"github.com/codecrafters-io/shell-starter-go/services/shell"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
-var commandRegistry = map[string]string{
-	"xyz":  "xyz",
-	"echo": "echo",
-	"cd":   "cd",
-	"exit": "exit",
-}
-
-func GetCommand(input string) (string, error) {
-	val, ok := commandRegistry[input]
-	if !ok {
-		return "", fmt.Errorf("%s: command not found", input)
-	}
-
-	return val, nil
-}
-
 func main() {
-	// REPL
-	for {
-		fmt.Print("$ ")
-
-		var command = ""
-		fmt.Scan(&command)
-
-		cmd, err := GetCommand(command)
-		if err != nil {
-			fmt.Print(err, "\n")
-
-		} else {
-			fmt.Print(cmd, "\n")
-		}
-	}
+	sh := shell.NewShell()
+	sh.Bootstrap()
 }
---
+++ services/shell/shell.go
@@ -0,0 +1,77 @@
+package shell
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+var commandRegistry = map[string]string{
+	"xyz":  "xyz",
+	"echo": "echo",
+	"cd":   "cd",
+	"exit": "exit",
+}
+
+type shell struct{}
+
+type Shell interface {
+	Bootstrap()
+	getCommand() (string, error)
+	handleCommand(command string)
+	handleGracefulShutdown()
+}
+
+func NewShell() *shell {
+	return &shell{}
+}
+
+func (s *shell) Bootstrap() {
+	for {
+		fmt.Print("$ ")
+
+		var command = ""
+		fmt.Scan(&command)
+
+		cmd, err := s.getCommand(command)
+		if err != nil {
+			fmt.Print(err, "\n")
+		} else {
+			s.commandHandler(cmd)
+		}
+	}
+}
+
+func (s *shell) getCommand(input string) (string, error) {
+	val, ok := commandRegistry[input]
+	if !ok {
+		return "", fmt.Errorf("%s: command not found", input)
+	}
+
+	return val, nil
+}
+
+func (s *shell) commandHandler(command string) {
+	switch command {
+	case commandRegistry["exit"]:
+		s.handleGracefulShutdown()
+	default:
+		fmt.Println("Unimplemented...")
+	}
+}
+
+func (s *shell) handleGracefulShutdown() {
+	// Find current running process
+	p, err := os.FindProcess(os.Getpid())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bye...")
+
+	// Emit Termination Signal
+	err = p.Signal(syscall.SIGTERM)
+	if err != nil {
+		fmt.Println("Failed to send termination signal.", err)
+	}
+}
