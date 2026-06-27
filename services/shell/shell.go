6d9ccd2c0a9276430ee196800f6af673e431a60a
Fikri <adhitamafikri.developer@gmail.com>
2026-06-27 18:12:53 +0700

    feat: working 'echo' builtin

--- services/shell/shell.go
+++ services/shell/shell.go
@@ -1,8 +1,10 @@
 package shell
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var commandRegistry = map[string]string{
@@ -12,51 +14,109 @@ var commandRegistry = map[string]string{
 	"exit": "exit",
 }
 
-type shell struct{}
+type shell struct {
+	reader *bufio.Reader
+}
 
 type Shell interface {
 	Bootstrap()
-	getCommand() (string, error)
-	handleCommand(command string)
+	getInput() string
+	getCommand(input string) (string, error)
+	getArgs(input string) []string
+	handleInput(input string)
+	handleEcho(args []string)
 	handleGracefulShutdown()
 }
 
 func NewShell() *shell {
-	return &shell{}
+	return &shell{
+		reader: bufio.NewReader(os.Stdin),
+	}
 }
 
 func (s *shell) Bootstrap() {
 	for {
-		fmt.Print("$ ")
-
-		var command = ""
-		fmt.Scan(&command)
-
-		cmd, err := s.getCommand(command)
+		input, err := s.getInput()
 		if err != nil {
 			fmt.Print(err, "\n")
 		} else {
-			s.commandHandler(cmd)
+			err = s.handleInput(input)
+
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
 
+func (s *shell) getInput() (string, error) {
+	fmt.Print("$ ")
+	input, err := s.reader.ReadString('\n')
+
+	if err != nil {
+		return "", fmt.Errorf("Something went wrong with the reader\n")
+	}
+
+	return input, nil
+}
+
 func (s *shell) getCommand(input string) (string, error) {
-	val, ok := commandRegistry[input]
+	splitResult := strings.Split(strings.TrimSpace(input), " ")
+	command := strings.TrimSpace(splitResult[0])
+
+	val, ok := commandRegistry[command]
 	if !ok {
-		return "", fmt.Errorf("%s: command not found", input)
+		return "", fmt.Errorf("%s: command not found", command)
 	}
 
 	return val, nil
 }
 
-func (s *shell) commandHandler(command string) {
+func (s *shell) getArgs(input string) []string {
+	splitResult := strings.Split(strings.TrimSpace(input), " ")
+
+	var args []string
+	if len(splitResult) > 1 {
+		args = splitResult[1:]
+	}
+
+	return args
+}
+
+// Note: Orchestrator function, responsible for: extracting command from input, extracting values from input, routing to appropriate handler
+func (s *shell) handleInput(input string) error {
+	command, err := s.getCommand(input)
+
+	if err != nil {
+		return err
+	}
+
+	args := s.getArgs(input)
+
 	switch command {
+	case commandRegistry["echo"]:
+		s.handleEcho(args)
 	case commandRegistry["exit"]:
 		s.handleGracefulShutdown()
 	default:
 		fmt.Println("Unimplemented...")
 	}
+
+	return nil
+}
+
+func (s *shell) handleEcho(args []string) {
+	length := len(args)
+
+	for idx, arg := range args {
+		if idx != length-1 {
+			fmt.Printf("%s ", arg)
+		} else {
+			fmt.Print(arg)
+		}
+	}
+
+	fmt.Print("\n")
 }
 
 func (s *shell) handleGracefulShutdown() {
